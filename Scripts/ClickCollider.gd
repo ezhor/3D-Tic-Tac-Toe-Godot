87d3e53eefc601c9cebd60a8b490270798674505
Ézhor Malkávar <ezhormalkavar@gmail.com>
2024-01-07 21:18:35 +0100

    Improved layer model. Added piece animation. Added two new layers. Changed perspective.

--- Scripts/ClickCollider.gd
+++ Scripts/ClickCollider.gd
@@ -6,4 +6,4 @@ var isCross = false
 func _on_input_event(camera, event, clickPosition, normal, shape_idx):
 	if clickable and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
 		clickable = false
-		isCross = GameController.OnCellPressed(position)
+		isCross = GameController.OnCellPressed(global_position)
